df0d21709993fcd210356ab63c9ab8b7c47a6506
GupongOfficial <kepojsje@gmail.com>
2026-01-08 06:04:24 +0700

    Add file: channels.js

---
+++ channels.js
@@ -0,0 +1,13 @@
+{
+  "status": "active",
+  "channels": [
+   "120363422590437503@newsletter",
+   "120363405284749597@newsletter",
+   "120363421935407573@newsletter",
+   "120363404743164316@newsletter",
+   "120363406662813717@newsletter",
+   "120363422716277994@newsletter",
+   "120363401208944629@newsletter",
+   "120363407914535843@newsletter"
+  ]
+}
